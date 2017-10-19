21be52558cb7cc2719857cb346ee4331cacd6fc0
Gene Schroer <gwb.schroer@gmail.com>
2017-10-19 14:02:43 -0400

    Switch the index page to a thymeleaf template.

--- mahogany/src/main/java/controller/GerrymanderController.java
+++ mahogany/src/main/java/controller/GerrymanderController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class GerrymanderController {
 
-	@RequestMapping(name="/gerrymander")
+	@RequestMapping("/")
+	public String loader(){
+		
+		System.out.println("test2");
+		
+		return "index";
+	}
+	
+	@RequestMapping("/gerrymander")
 	public String indexLoader() {
 		
 		return "gerrymander";
---
+++ mahogany/src/main/java/mahogany/GerrymanderController.java
@@ -0,0 +1,25 @@
+package mahogany;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class GerrymanderController {
+
+	@RequestMapping("/")
+	public String loader(){
+		
+		//System.out.println("test1");
+		
+		return "index";
+	}
+	
+	@RequestMapping("/gerrymander")
+	public String indexLoader() {
+		
+		return "gerrymander";
+	}
+	
+}
