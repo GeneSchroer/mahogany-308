534ce5cc1dd79bb711f98ee2b658a91618122526
Gene Schroer <gwb.schroer@gmail.com>
2017-11-05 14:02:00 -0500

    Update the redistrict page to have its own page and map managers, as well as to be able to click on districts to highlight them

--- mahogany/target/classes/static/leaflet/MapUtils.js
+++ mahogany/target/classes/static/leaflet/MapUtils.js
@@ -1,3 +1,45 @@
 function changeColorOfFeatureTo(){}
 function setToSelectedSuperdistrict(){}
 function setToUnselectedSuperdistrict(){}
+
+define([], function(){
+	
+	
+	return {
+		redistrictDistrictLayerStyle: function(mapData){
+			return function(feature){
+				//console.log(mapData);
+				return{
+					weight: 0.5,
+					color: "white",
+					fillColor: "#464646",
+					dataArray: ' ',
+					fillOpacity: 0.8
+				};
+			};
+		},
+		redistrictDistrictLayerHighlightStyle: function(mapData){
+				//console.log(mapData);
+				return{
+					weight: 0.5,
+					color: "white",
+					fillColor: "#949394",
+					dataArray: ' ',
+					fillOpacity: 0.8
+				};
+			
+		},
+		redistrictCreateSuperdistrict: function(mapData){
+			return{
+				weight: 0.5,
+				color: "white",
+				fillColor: "#b4a1a1",
+				dataArray: ' ',
+				fillOpacity: 0.8
+			};
+		}
+		
+		
+	};
+	
+});
