4e558cc8bbb6e32327c79f85c1fb9f44e768cfe3
Gene Schroer <gwb.schroer@gmail.com>
2017-11-14 18:10:18 -0500

    turn GerrymanderHelper into a Spring Component class and autowire the repository classes to it.

--- mahogany/src/main/java/mahogany/GerrymanderController.java
+++ mahogany/src/main/java/mahogany/GerrymanderController.java
@@ -6,37 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.apache.commons.fileupload.FileItem;  
-import org.apache.commons.fileupload.FileItemFactory;  
-import org.apache.commons.fileupload.FileUploadException;  
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;  
-import org.apache.commons.fileupload.servlet.ServletFileUpload; 
 import org.springframework.web.multipart.MultipartFile;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Controller
 public class GerrymanderController {
 
-	@Autowired
-	private StateNamesRepository stateNamesRepository;
+	//@Autowired
+	//private StateNamesRepository stateNamesRepository;
+	
+	//@Autowired
+	//private DistrictsRepository districtsRepository;
 	
 	@Autowired
-	private DistrictsRepository districtsRepository;
-	
-	@Autowired 
-	DistrictDetailsRepository districtDetailsRepository;
-	
-	GerrymanderHelper helper = new GerrymanderHelper();
+	GerrymanderHelper helper; //= new GerrymanderHelper();
 	
 	@RequestMapping("/")
 	public String loader(){
@@ -61,7 +43,7 @@ public class GerrymanderController {
 	
 	@RequestMapping("/admin/upload")
 	public String uploadFile(@RequestParam("file")MultipartFile[] files) {
-		helper.setDistrictsRepository(districtsRepository);
+		//helper.setDistrictsRepository(districtsRepository);
 
 		helper.uploadFiles(files);
 		
@@ -87,63 +69,5 @@ public class GerrymanderController {
 		return "gerrymander";
 	}
 	
-	public ArrayList<Districts> convertJsonToDistricts(ObjectNode json){
-		int count=0;
-		ArrayList<Districts> districtList = new ArrayList<Districts>();
-		ArrayNode featuresArray = (ArrayNode)json.get("features");
-		Iterator<JsonNode> features = featuresArray.iterator();
-		while(features.hasNext()) {
-			ObjectNode properties = (ObjectNode)features.next().get("properties");
-			//Districts district = new Districts();
-			System.out.println(properties.get("startcong").asInt());
-			//district.setCongress(properties.get("startcong").asInt());
-			//district.setNumber(properties.get("district").asInt());
-			//System.out.println(properties.get("statename").asText());
-			//System.out.println(count++);
-			String stateName = properties.get("statename").asText();
-			int startCong = properties.get("startcong").asInt();
-			int endCong = properties.get("endcong").asInt();
-			int number = properties.get("district").asInt();
-			StateNames stateNames = stateNamesRepository.findFirstByName(stateName);
-			if(stateNames == null) {
-			
-				System.out.println("creating new state");
-				stateNames = new StateNames();
-				stateNames.setName(stateName);
-				stateNamesRepository.save(stateNames);
-				
-			}
-			
-			for(int congress = startCong; congress<=endCong; congress = congress +1) {
-				DistrictDetails districtDetails = districtDetailsRepository.findByStateNameAndNumberAndCongress(stateName, number, congress);
-				if(districtDetails == null) {
-					districtDetails = new DistrictDetails();
-					districtDetails.setStateId(stateNames.getId());
-					districtDetails.setNumber(number);
-					districtDetails.setCongress(congress);
-					districtDetailsRepository.save(districtDetails);
-					//districtDetails.setStateName(stateNames);
-				}
-				
-				//System.out.println("State Name: " + districtDetails.getStateName().getName());
-				Districts districts = districtsRepository.findByStateNameAndNumberAndCongress(stateName, number, congress);
-				if (districts == null) {
-					districts = new Districts();
-					districts.setDetailsId(districtDetails.getId());
-					districtsRepository.save(districts);
-				}
-				ObjectNode memberObject = (ObjectNode)properties.get("member");
-				ObjectNode electionObject = (ObjectNode)properties.get("election");
-				
-				ObjectNode memberAndCongressObject = (ObjectNode)memberObject.get(String.valueOf(congress));
-				ObjectNode electionAndCongressObject = (ObjectNode)electionObject.get(String.valueOf(congress));
-			}
-			
-			
-			//districts.add(district);
-			
-		}
-		return districtList;
-	}
 	
 }
