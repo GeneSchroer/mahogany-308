bcef010f65efe8b3020a15b9c2f4799aa5d4c4e2
Gene Schroer <gwb.schroer@gmail.com>
2017-11-25 20:10:05 -0500

    Create copy of Gerrymander Page and Map Builders

---
+++ mahogany/target/classes/static/leaflet/gerrymander/GerrymanderMapBuilder2.js
@@ -0,0 +1,310 @@
+define([
+	"dojo/_base/declare",
+	"dojo/on",
+	"dojo/topic",
+	"dojo/dom-style",
+	"dojo/request",
+	"leaflet/gerrymander/metrics/ElectionData",
+	"leaflet/gerrymander/metrics/EfficiencyGap",
+	
+	],function(declare, on, topic, domStyle, request, ElectionData, EfficiencyGap){
+	var METRIC_DEFAULT_MODE = "defaultMode";
+	var METRIC_EFFICIENCY_GAP = "efficiencyGap";
+	
+	var MAP_MODE_STATE = "state";
+	var MAP_MODE_DISTRICT = "district";
+	
+	
+	function stateLayerStyle(mapData){
+		return {
+			weight: 1,
+			color: "green",
+			fillColor: "lime",
+			fillOpacity: 0.1
+		};
+	}
+		
+	function setStateLayerEvents(mapData){
+		return function(feature, layer){
+	
+			layer.on({
+				mouseover: function(e){
+					layer = e.target;
+					layer.setStyle({
+						weight: 2,
+						color: 'brown',
+						dataArray:'',
+						fillOpacity: 0.7
+					});
+				},
+				mouseout:function(e){
+					mapData.stateLayer.resetStyle(e.target);
+				},
+				click: function(e){
+					mapData.districtLayer.clearLayers();
+					mapData.mode = MAP_MODE_DISTRICT;
+					console.log(e.target.feature);
+					mapData.state = e.target.feature.properties.name;
+					mapData.map.fitBounds(e.target.getBounds());
+					for(control in mapData.districtMapControls){
+						mapData.districtMapControls[control].enable();
+					}
+					getDistrictsRequest(mapData);
+				}
+			});
+		}
+	}
+	
+	function loadDistrictData(mapData){
+		if(mapData.metricMode == METRIC_DEFAULT_MODE){
+			getElectionDataRequest(mapData);
+		}
+		else if(mapData.metricMode == METRIC_EFFICIENCY_GAP){
+			efficiencyGapRequest(mapData);
+		}
+	}
+	function getElectionDataRequest(mapData){
+		request("electionDataRequest",{
+			method: "GET",
+			query:{
+				state: mapData.state,
+				year: mapData.year
+			}, 
+			handleAs: "json"
+		}).response.then(function(success){
+			console.log(success.data);
+			mapData.metricData.defaultMode = success.data;
+			setElectionDataLayer(mapData);
+		});
+	}
+	
+	function getDistrictsRequest(mapData){
+		
+		
+		
+		request("districtBoundariesRequest", {
+			method: "POST",
+			data: {
+				state: mapData.state,
+				year: mapData.year
+			},
+			handleAs: 'json'
+		}).response.then(function(success){
+			console.log(success.data);
+			//topic.publish("gerrymander/getDistricts/success", success);
+			var districtData = success.data;
+			mapData.districtLayer.clearLayers();
+			mapData.districtLayer.addData(districtData);
+			loadDistrictData(mapData);
+		});
+	}
+	function setStateLayersRequest(mapData){
+		request("stateData.json",{
+			method: "GET",
+			handleAs: "json"
+		}).response.then(function(success){
+			var stateData = success.data;
+			console.log("Add states to the layer");
+			mapData.stateLayer.addData(stateData);
+			
+		});
+	}
+	
+	
+	
+	var GerrymanderMapManager = L.Class.extend({
+		
+		_mapData: null,
+		initialize: function(map){
+			this._constructGerrymanderMap(map);
+			this._mapData.mode = MAP_MODE_STATE;
+		},
+		
+		_constructGerrymanderMap:function(map){
+			this._mapData = {};
+			
+			this._mapData.map = this._createMap(map);
+			
+			console.log("Create district layer")
+			this._mapData.districtLayer = this._createDistrictLayer(this._mapData);
+			this._mapData.districtLayer.addTo(this._mapData.map);
+			
+			console.log("Create state layer")
+			this._mapData.stateLayer = this._createStateLayer(this._mapData);
+			this._mapData.stateLayer.addTo(this._mapData.map);
+			
+			this._mapData.mode = MAP_MODE_STATE;
+			
+			this._mapData.districtMapControls = {}
+			
+			console.log("Create zoom out button")
+			this._mapData.districtMapControls.zoomOutBtn = this._createZoomOutBtn(this._mapData);
+			this._mapData.districtMapControls.zoomOutBtn.addTo(this._mapData.map);
+			
+			
+			this._mapData.districtMapControls.dataDisplay = this._createDataDisplay(this._mapData);
+			this._mapData.districtMapControls.dataDisplay.addTo(this._mapData.map);
+			
+			this._mapData.metricMode=METRIC_DEFAULT_MODE;
+			this._mapData.metricData = {};
+			
+			setStateLayersRequest(this._mapData);
+		},	
+		_createMap: function(map){
+			domStyle.set(map, "width", "100%");
+			domStyle.set(map, "height", "100%");
+			domStyle.set(map, "position", "absolute");
+			return L.map(map, {
+				zoomControl: false,
+				/*dragging: false,*/
+				scrollWheelZoom: false,
+			}).setView([32, -80], 4);
+		},
+		_createDistrictLayer: function(mapData){
+			var districtLayer = L.geoJson(null, {onEachFeature:null
+				});
+
+			return districtLayer;
+		},
+		_createStateLayer: function(mapData){
+			var stateLayer = L.geoJson(null, {
+					style: stateLayerStyle(),
+					onEachFeature: setStateLayerEvents(mapData) 
+				});
+			return stateLayer;
+		},
+		_createZoomOutBtn:function(mapData){
+			var zoomOutBtn = L.control({position:'topleft'});
+			zoomOutBtn.onAdd = function(map){
+				this._div=L.DomUtil.create('button', 'zoomOut');
+				this._div.innerHTML = "ZoomOut";
+				this.disable();
+				return this._div;
+			};
+			zoomOutBtn.disable = function(){
+				mapData.mode=MAP_MODE_STATE;
+				domStyle.set(this._div, "display", "none");
+				mapData.map.setView([38, -88], 4, true);
+				L.DomEvent.off(zoomOutBtn);
+			};
+			zoomOutBtn.enable = function(){
+				domStyle.set(this._div, "display", "block");
+				L.DomEvent.on(this._div, 'click', function(evt){
+					mapData.districtLayer.clearLayers();
+					L.Util.setOptions(mapData.districtLayer, {style:null});
+					mapData.metricData = {};
+					for(control in mapData.districtMapControls){
+						mapData.districtMapControls[control].disable();
+					}
+					
+				});
+			};
+			return zoomOutBtn;
+		},
+		_createDataDisplay: function(mapData){
+			var dataDisplay = L.control({position:'topright'});
+			dataDisplay.onAdd = function(map){
+				this._div = L.DomUtil.create('div', 'dataDisplay');
+				this._div.innerHTML = "Hello";
+				this.disable();
+				return this._div;
+			};
+			dataDisplay.update = function (string) {
+			    this._div.innerHTML = string ? string : "Data Not Available";
+			};
+			dataDisplay.disable = function(){
+				domStyle.set(this._div, "display", "none");
+			};
+			dataDisplay.enable = function(){
+				domStyle.set(this._div, "display", "block");
+			};
+			
+			return dataDisplay;
+			
+		},
+		setYear: function(year){
+			console.log(this);
+			this._mapData.year = year;
+			this._updateMap();
+		},
+		_updateMap: function(){
+			this._mapData.metricData={};
+			getDistrictsRequest(this._mapData);
+		},
+		setMetricToEfficiencyGap: function(){
+			this._mapData.metricMode = METRIC_EFFICIENCY_GAP;
+			
+			if(!this._mapData.metricData.efficiencyGap){
+				efficiencyGapRequest(this._mapData);
+			}
+			else{
+				setEfficiencyGapLayer(this._mapData);
+			}
+		},
+		setMetricToDefaultMode: function(){
+			this._mapData.metricMode = METRIC_DEFAULT_MODE;
+			if(!this._mapData.metricData.defaultMode){
+				getElectionDataRequest(this._mapData);
+			}
+			else{
+				setElectionDataLayer(this._mapData);
+			}
+		}
+	
+		
+		
+		
+		
+	});
+	
+	function efficiencyGapRequest(mapData){
+
+		request("/efficiencyGapRequest",{
+			method: "GET",
+			query: {
+				state: mapData.state,
+				year: mapData.year
+			},
+			handleAs: "json"
+		}).response.then(function(success){
+			console.log(success.data);
+			mapData.metricData.efficiencyGap = success.data;
+			setEfficiencyGapLayer(mapData);
+		});
+
+	}
+	
+
+	
+	
+	function setEfficiencyGapLayer(mapData){
+		
+			
+		
+			L.Util.setOptions(mapData.districtLayer, {style:null});
+			L.Util.setOptions(mapData.districtLayer, { style: EfficiencyGap.setStyle(mapData)	});
+			
+			mapData.districtLayer.eachLayer(EfficiencyGap.setEvents(mapData));
+			
+			
+			mapData.districtLayer.setStyle(EfficiencyGap.setStyle(mapData));
+	}
+	
+	function setElectionDataLayer(mapData){
+		
+		mapData.districtLayer.setStyle(ElectionData.setStyle(mapData));
+		L.Util.setOptions(mapData.districtLayer, {style:null});
+		L.Util.setOptions(mapData.districtLayer, {style: ElectionData.setStyle(mapData)});
+		
+		mapData.districtLayer.eachLayer(ElectionData.setEvents(mapData));
+		
+	}
+	
+		
+	
+	return {
+		create: function(map){
+			return new GerrymanderMapManager(map);
+		}
+	};
+});
---
+++ mahogany/target/classes/static/leaflet/gerrymander/GerrymanderPageBuilder2.js
@@ -0,0 +1,142 @@
+define([
+	"dojo/_base/declare", 
+	"dojo/on", 
+	"dojo/dom-construct", 
+	"dojo/dom-style", 
+	"dojo/request", 
+	"dojo/topic", 
+	"leaflet/gerrymander/GerrymanderMapBuilder2", 
+	"dijit/registry"
+	], function(declare, on, domConstruct, domStyle, request, topic, GerrymanderMapBuilder, registry){
+	
+	
+	var GerrymanderPageManager = declare(null, {
+		_pageElements: null,
+		
+		constructor: function(builder){
+			this._pageElements = builder;
+			
+			
+			this._pageElements.mapManager = GerrymanderMapBuilder.create(this._pageElements.map);
+			this._initializeYearSelector(this._pageElements);
+			this._initializeRepTable(this._pageElements);
+			this._initializePartyCheckbox(this._pageElements);
+			this._initializeDefaultModeButton(this._pageElements);
+			this._initializeEfficiencyGapButton(this._pageElements);
+		},
+		
+		_initializeYearSelector: function(pageElements){
+			var yearSelector = pageElements.yearSelector;
+			pageElements.mapManager.setYear(pageElements.yearSelector.value);
+			on(registry.byId(pageElements.yearSelector), "change", function(){
+				console.log("selected");
+				pageElements.mapManager.setYear(pageElements.yearSelector.value);
+			//	pageElements.mapManager.updateMap();
+			});
+			
+			request("/getYears",{
+					method:"GET",
+					handleAs: "json"
+				}).response.then(function(success){
+					var yearList = success.data;
+					console.log(yearList);
+					
+					//yearList.sort();
+					
+					for(year in yearList){
+						var option = domConstruct.create("option", {label:yearList[year], value: yearList[year]});
+						yearSelector.addOption(option);
+					}
+					if(year[0]){
+						pageElements.mapManager.setYear(yearList[0]);
+					}
+				});
+			
+		},
+		_initializeRepTable: function(pageElements){
+			var table = pageElements.repTable;
+			var header = domConstruct.create("tr", {}, table);
+			var districts;
+			var count;
+			domConstruct.create("th", {innerHTML:"Name"}, header);
+			domConstruct.create("th", {innerHTML:"District"}, header);
+			domConstruct.create("th", {innerHTML:"Party"}, header);
+			topic.subscribe("gerrymander/getDistricts/success", function(response){
+				domConstruct.empty(table);
+				header = domConstruct.create("tr", {}, table);
+				domConstruct.create("th", {innerHTML:"Name"}, header);
+				domConstruct.create("th", {innerHTML:"District"}, header);
+				domConstruct.create("th", {innerHTML:"Party"}, header);
+				districts = response.data.features;
+				//console.log(districts);
+				for(count = 0; count < districts.length; count = count + 1){
+					var member = districts[count].properties.member[pageElements.yearSelector.value];
+					//console.log(member);
+					for (column in member){
+						//console.log(column);
+						var row = domConstruct.create("tr", {}, table);
+						domConstruct.create("td", {innerHTML: member[column].name}, row);
+						domConstruct.create("td", {innerHTML: member[column].district}, row);
+						domConstruct.create("td", {innerHTML: member[column].party}, row);
+					}
+				}
+			});
+		},
+		_initializePartyCheckbox: function(){
+			
+		},
+		_initializeDefaultModeButton: function(pageElements){
+			var defaultModeRadioBtn = pageElements.defaultModeRadioBtn;
+			on(defaultModeRadioBtn, "click", function(e){
+				pageElements.mapManager.setMetricToDefaultMode();
+			});
+		},
+		_initializeEfficiencyGapButton: function(pageElements){
+			var efficiencyGapRadioBtn = pageElements.efficiencyGapRadioBtn;
+			on(efficiencyGapRadioBtn, "click", function(e){
+				pageElements.mapManager.setMetricToEfficiencyGap();
+			});
+		}
+		
+		
+		
+	});
+	
+	
+	return {
+		builder: {},
+		addMap: function(map){
+			this.builder.map = map;
+			return this;
+		},
+		addYearSelector:function(select){
+			this.builder.yearSelector = select;
+			return this;
+		},
+		addRepTable: function(table){
+			this.builder.repTable = table;
+			return this;
+		},
+		addPartyCheckbox: function(checkbox){
+			this.buidler.partyCheckbox = checkbox;
+			return this;
+		},
+		addMetricSelectForm: function(form){
+			this.builder.metricSelectForm = form;
+			return this;
+		},
+		addDefaultModeRadioButton: function(button){
+			this.builder.defaultModeRadioBtn = button;
+			return this;
+		},
+		addEfficiencyGapRadioButton: function(button){
+			this.builder.efficiencyGapRadioBtn = button;
+			return this;
+		},
+		build: function(){
+			//create copy of builder
+			var builderCopy = this.builder;
+			return new GerrymanderPageManager(builderCopy);
+		}
+	} 
+});
